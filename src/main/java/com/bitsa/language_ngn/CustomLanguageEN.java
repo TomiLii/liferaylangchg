3d0533199e8dbee9c6d6568d8aae419069d40d9d
TomiLii <tomasz.laszuk@gmail.com>
2017-10-03 14:21:48 +0200

    initial commit

---
+++ src/main/java/com/bitsa/language_ngn/CustomLanguageEN.java
@@ -0,0 +1,35 @@
+package com.bitsa.language_ngn;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+
+import com.liferay.portal.kernel.language.UTF8Control;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+@Component(
+		 property = { "language.id=en_US" }, 
+		 service = ResourceBundle.class
+          )
+public class CustomLanguageEN extends ResourceBundle 
+{
+	
+ResourceBundle bundle = ResourceBundle.getBundle("content.Language_en", UTF8Control.INSTANCE);
+private Log _log = LogFactoryUtil.getLog(CustomLanguageEN.class);
+
+	@Override
+	protected Object handleGetObject(String key) 
+        {
+		_log.info("Reading key: " + key + ", value: " + bundle.getObject(key) + ", from " + bundle.getBaseBundleName());
+		return bundle.getObject(key);
+	}
+
+	@Override
+	public Enumeration<String> getKeys() 
+        {
+		return bundle.getKeys();
+	}
+
+}
